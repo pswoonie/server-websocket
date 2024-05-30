58430683a77f085bbe3dd75e495f93c485717dac
pswoonie <swp8111@gmail.com>
2024-05-30 19:04:28 +0900

    [UPDATE] save

--- src/server.js
+++ src/server.js
@@ -7,11 +7,27 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const handleListen = () => {
-  console.log("listening...");
+  console.log("Server Start: listening...");
 };
 
-wss.on("connection", (socket) => {
-  socket.send("hello");
-});
-
 server.listen(3000, handleListen);
+
+//TODO: this part should be connected to database
+//TODO: change later
+let rooms = [];
+
+// ==============================================
+
+wss.on("connection", (socket) => {
+  // Show on connection with client
+  console.log("Connected to Client!!!");
+
+  // Get messages from clients
+  socket.on("message", (data, isBinary) => {
+    const message = isBinary ? data : data.toString();
+    socket.send(message);
+  });
+
+  // Check if connection is gone
+  socket.on("close", () => console.log("Disconnected from client!!!"));
+});
