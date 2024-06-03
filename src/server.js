5263be7c0fefdca1bfb6bdd8a69af612cc85cc3b
pswoonie <swp8111@gmail.com>
2024-06-03 17:05:51 +0900

    [UPDATE] setup socket io

--- src/server.js
+++ src/server.js
@@ -1,63 +1,26 @@
 import express from "express";
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = SocketIO(server);
 
-const handleListen = () => {
-  console.log("Server Start: listening...");
-};
+server.listen(3000, () => console.log("Server Start: listening..."));
 
-server.listen(3000, handleListen);
-
-//TODO: this part should be connected to database
-//TODO: change later
-let clients = [];
-
-// ==============================================
-
-function addSockets(socket) {
-  const result = clients.filter(
-    (c) => c.rid === socket.roomId && c.uid === socket.userId
-  );
-
-  if (result !== null || result !== undefined || result.length == 0) {
-    const newRoom = { socket: socket, rid: socket.roomId, uid: socket.userId };
-    clients.push(newRoom);
-  }
-
-  return;
-}
-
-function sendMessage(message) {
-  const result = clients.filter(
-    (c) => c.rid === message.rid && c.uid !== message.uid
-  );
-
-  result.forEach((client) => client.socket.send(JSON.stringify(message)));
-  console.log("Message Sent!!!");
-}
-
-wss.on("connection", (socket) => {
+io.on("connection", (socket) => {
   // Show on connection with client
   console.log("Connected to Client!!!");
 
-  // Get messages from clients
-  socket.on("message", (data, isBinary) => {
-    const jsonString = isBinary ? data : data.toString();
-    const message = JSON.parse(jsonString);
-    socket["roomId"] = message.rid;
-    socket["userId"] = message.uid;
+  socket.on("JOIN_ROOM", (client) => {
+    socket.join(client.rid);
+    socket.to(client.rid).emit("CONNECTED", client);
+  });
 
-    // Add sockets
-    addSockets(socket);
-
-    // Distribute messages
-    sendMessage(message);
+  socket.on("FROM_CLIENT", (message) => {
+    socket.to(message.rid).emit("FROM_SERVER", message);
   });
 
   // Check if connection is gone
-  socket.on("close", () => console.log("Disconnected from client!!!"));
+  socket.on("disconnect", () => console.log("Disconnected from client!!!"));
 });
